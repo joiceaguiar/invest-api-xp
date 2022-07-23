f94aecd4ed2a395bcfdc2d118c06df051dd3d183
Joice <joicemdsaguiar@gmail.com>
2022-07-23 20:33:48 -0300

    :sparkles: feat: added getByClientId controller

--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -18,7 +18,18 @@ const createWithdrawal = async (req, res) => {
   }
 };
 
+const getByClientId = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { status, response } = await accountService.getByClientId(id);
+    return res.status(status).json(response);
+  } catch (error) {
+    return res.status(500).send('Internal Server Error');
+  }
+};
+
 module.exports = {
   createDeposit,
   createWithdrawal,
+  getByClientId,
 };
