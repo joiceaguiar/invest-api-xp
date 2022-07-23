efc5b3c316a78e372e6a2a243990ff5efb1b5dde
Joice <joicemdsaguiar@gmail.com>
2022-07-23 20:58:48 -0300

    :sparkles: feat: added getByClientId assetController

---
+++ src/controllers/assetController.js
@@ -0,0 +1,15 @@
+const assetService = require('../services/assetService');
+
+const getByClientId = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { status, response } = await assetService.getByClientId(id);
+    return res.status(status).json(response);
+  } catch (error) {
+    return res.status(500).send('Internal Server Error');
+  }
+};
+
+module.exports = {
+  getByClientId,
+};
