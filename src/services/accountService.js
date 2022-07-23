352c6bf6e390dfc1b741b80c84dccd2bf3aed255
Joice <joicemdsaguiar@gmail.com>
2022-07-23 20:30:36 -0300

    :sparkles: feat: added createWithdrawal service

--- src/services/accountService.js
+++ src/services/accountService.js
@@ -15,7 +15,22 @@ const createDeposit = async ({ clientId, value }) => {
   return { status: 201, response: { CodCliente: clientId, Valor: value } };
 };
 
+const createWithdrawal = async ({ clientId, value }) => {
+  const client = await Client.findOne({ where: { id: clientId } });
+
+  const newBalance = Number(client.balance) - Number(value);
+
+  if (value <= 0 || value > client.balance) {
+    return { status: 400, response: { message: 'The value you entered is not valid' } };
+  };
+
+  const updateBalanceClient = await Client.update(
+    { balance: newBalance }, { where: { id: clientId } });
+
+  return { status: 201, response: { CodCliente: clientId, Valor: value } };
+};
+
 module.exports = {
   createDeposit,
-  
+  createWithdrawal,
 };
