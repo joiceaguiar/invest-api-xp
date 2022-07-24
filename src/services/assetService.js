975212f8cb1996bf4f4863151f5948ade48e9549
Joice <joicemdsaguiar@gmail.com>
2022-07-24 18:25:13 -0300

    :bug: fix: added assetService changes

--- src/services/assetService.js
+++ src/services/assetService.js
@@ -1,23 +1,24 @@
 const { Asset, ClientAsset } = require('../database/models');
 
 const getByClientId = async (clientId) => {
-  const client = await ClientAsset.findByPk(clientId);
-  const asset = await Asset.findOne();
+  const client = await ClientAsset.findAll( { where: { clientId } });
 
-  if (!client) {
+  if (!client.length) {
     return { status: 404, response: { message: 'Client not found' } };
   };
 
-  return { 
-    status: 200, 
-    response: [{ 
-      CodCliente: clientId,
-      CodAtivo: client.assetId,
-      QtdeAtivo: client.quantity,
+  const dataValues = await Promise.all(client.map(async(data) => {
+    const asset = await Asset.findOne({ where: { id: data.assetId } });
+    const datas = {
+      CodCliente: data.clientId,
+      CodAtivo: data.assetId,
+      QtdeAtivo: data.quantity,
       Valor: asset.value
-    }]
-  };
-};
+    }
+    return datas;
+  }));
+
+  return { status: 200, response: dataValues } };
 
 const getByAssetId = async (assetId) => {
   const asset = await Asset.findOne({ where: { id: assetId } });
