442c55c4c371b64a5c7ab249144bd26adf9398ca
Joice <joicemdsaguiar@gmail.com>
2022-07-23 21:00:16 -0300

    :sparkles: feat: added getByAssetId assetService

--- src/services/assetService.js
+++ src/services/assetService.js
@@ -19,6 +19,24 @@ const getByClientId = async (clientId) => {
   };
 };
 
+const getByAssetId = async (assetId) => {
+  const asset = await Asset.findOne({ where: { id: assetId } });
+
+  if (!asset) {
+    return { status: 404, response: { message: 'Asset does not exist' } };
+  };
+
+  return {
+    status: 200,
+    response: { 
+      CodAtivo: assetId, 
+      QtdeAtivo: asset.quantity,
+      Valor: asset.value
+    }
+  };
+};
+
 module.exports = {
   getByClientId,
+  getByAssetId,
 };
