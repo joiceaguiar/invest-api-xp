06fd6e5cfecf72bc409908f7f292d3750bf77053
Joice <joicemdsaguiar@gmail.com>
2022-07-24 18:24:47 -0300

    :bug: fix: added accountService changes

--- src/services/accountService.js
+++ src/services/accountService.js
@@ -3,20 +3,33 @@ const { Client } = require('../database/models');
 const createDeposit = async ({ clientId, value }) => {
   const client = await Client.findOne({ where: { id: clientId } });
 
+  if (!client) {
+    return res.status(404).json({ message: 'Client does not exist' }); 
+  };
+
   const newBalance = Number(value) + Number(client.balance);
 
   if (value <= 0) {
     return { status: 400, response: { message: 'The value you entered is not valid' } };
   };
 
-  const updateBalanceClient = await Client.update(
-    { balance: newBalance }, { where: { id: clientId } });
+  await Client.update({ balance: newBalance }, { where: { id: clientId } });
   
-  return { status: 201, response: { CodCliente: clientId, Valor: value } };
+  return {
+    status: 201,
+    response: {
+      CodCliente: clientId,
+      Valor: value
+    }
+  };
 };
 
 const createWithdrawal = async ({ clientId, value }) => {
   const client = await Client.findOne({ where: { id: clientId } });
+
+  if (!client) {
+    return res.status(404).json({ message: 'Client does not exist' }); 
+  };
 
   const newBalance = Number(client.balance) - Number(value);
 
@@ -24,10 +37,15 @@ const createWithdrawal = async ({ clientId, value }) => {
     return { status: 400, response: { message: 'The value you entered is not valid' } };
   };
 
-  const updateBalanceClient = await Client.update(
-    { balance: newBalance }, { where: { id: clientId } });
+  await Client.update({ balance: newBalance }, { where: { id: clientId } });
 
-  return { status: 201, response: { CodCliente: clientId, Valor: value } };
+  return {
+    status: 201,
+    response: {
+      CodCliente: clientId,
+      Valor: value
+    }
+  };
 };
 
 const getByClientId = async (clientId) => {
@@ -37,7 +55,13 @@ const getByClientId = async (clientId) => {
     return { status: 404, response: { message: 'Client does not exist' } };
   };
   
-  return { status: 200, response: { CodCliente: clientId, Saldo: client.balance } };
+  return {
+    status: 200,
+    response: {
+      CodCliente: clientId,
+      Saldo: client.balance
+    }
+  };
 };
 
 module.exports = {
