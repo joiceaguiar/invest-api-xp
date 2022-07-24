54775be5ad248b40ffe07c27c651869f2db03eac
Joice <joicemdsaguiar@gmail.com>
2022-07-24 18:23:43 -0300

    :bug: fix: fixed bug investments router

--- src/routes/investments/index.js
+++ src/routes/investments/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 
 const investmentController = require('../../controllers/investmentController');
-const clientValidation = require('../../middlewares/clientMiddleware');
-const assetValidation = require('../../middlewares/assetMiddleware');
+const investmentValidation = require('../../middlewares/investmentMiddleware');
 
 const investmentsRouter = express.Router();
 
-investmentsRouter.post('/comprar', clientValidation, assetValidation, investmentController.createPurchase);
-investmentsRouter.post('/vender', clientValidation, assetValidation, investmentController.createSale);
+investmentsRouter.post('/comprar', investmentValidation, investmentController.createPurchase);
+investmentsRouter.post('/vender', investmentValidation, investmentController.createSale);
 
 module.exports = investmentsRouter;
