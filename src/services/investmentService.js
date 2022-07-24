6d93b83ac078a064a8dbd3d0c60c3eb7e54ae9cd
Joice <joicemdsaguiar@gmail.com>
2022-07-24 18:25:35 -0300

    :bug: fix: added investmentService changes

--- src/services/investmentService.js
+++ src/services/investmentService.js
@@ -3,55 +3,88 @@ const { Asset, Client, ClientAsset } = require('../database/models');
 const createPurchase = async ({ clientId, assetId, quantity }) => {
   const client = await Client.findOne({ where: { id: clientId } });
   const asset = await Asset.findOne({ where: { id: assetId } });
-  const clientAsset = await ClientAsset.findOne({ where: { clientId, assetId } });
 
-  const totalValue = quantity * asset.value;
-  const newQuantity = Number(asset.quantity) - Number(quantity);
-  const newBalance = Number(client.balance) - Number(totalValue);
+  if (!client) {
+    return res.status(404).json({ message: 'Client does not exist' }); 
+  };
+
+  if (!asset) {
+    return res.status(404).json({ message: 'Asset does not exist' }); 
+  };
+
+  const clientWallet = await ClientAsset.findOne({ where: { clientId, assetId } });
 
   if (quantity > asset.quantity) {
     return { status: 400, response: { message: 'Quantity cannot be greater than available' } };
   };
 
+  if (!clientWallet) {
+    await ClientAsset.create({ clientId, assetId, quantity });
+  } else {
+    const updateWallet = Number(clientWallet.quantity) + Number(quantity);
+    await ClientAsset.update({ quantity: updateWallet }, { where: { clientId, assetId } });
+  };
+
+  const totalValue = quantity * asset.value;
+  const newQuantity = Number(asset.quantity) - Number(quantity);
+  const newBalance = Number(client.balance) - Number(totalValue);
+
   if (totalValue > client.balance) {
     return { status: 400, response: { message: 'The amount cannot be greater than clients balance' } };
   };
 
-  const updateAsset = await Asset.update(
-    { quantity: newQuantity }, { where: { id: assetId } });
+  await Asset.update({ quantity: newQuantity }, { where: { id: assetId } });
+  await Client.update({ balance: newBalance }, { where: { id: clientId } });
 
-  const updateBalanceClient = await Client.update(
-    { balance: newBalance }, { where: { id: clientId } });
-
-  const createWallet = await ClientAsset.create({ clientId, assetId, quantity });
-
-  return { status: 201, response: { codCliente: clientId, codAtivo: assetId, qtdeAtivo: quantity } };
+  return {
+    status: 201,
+    response: {
+      codCliente: clientId,
+      codAtivo: assetId,
+      qtdeAtivo: quantity
+    }
+  };
 };
 
 const createSale = async ({ clientId, assetId, quantity }) => {
   const client = await Client.findOne({ where: { id: clientId } });
   const asset = await Asset.findOne({ where: { id: assetId } });
-  const clientAsset = await ClientAsset.findOne({ where: { clientId, assetId } });
+
+  if (!client) {
+    return res.status(404).json({ message: 'Client does not exist' }); 
+  };
+
+  if (!asset) {
+    return res.status(404).json({ message: 'Asset does not exist' }); 
+  };
+
+  const clientWallet = await ClientAsset.findOne({ where: { clientId, assetId } });
+
+  if (!clientWallet) {
+    return { status: 404, response: { message: 'Client does not has assets' } }; 
+  };
 
   const totalValue = quantity * asset.value;
-  const newQuantityWallet = Number(clientAsset.quantity) - Number(quantity);
   const newBalance = Number(client.balance) + Number(totalValue);
   const newQuantity = Number(asset.quantity) + Number(quantity);
+  const updateWallet = Number(clientWallet.quantity) - Number(quantity);
 
-  if (quantity > clientAsset.quantity) {
+  if (quantity > clientWallet.quantity) {
     return { status: 400, response: { message: 'Quantity cannot be greater than available' } };
   };
 
-  const updateAssetWallet = await ClientAsset.update(
-    { quantity: newQuantityWallet }, { where: { clientId, assetId } });
+  await Asset.update({ quantity: newQuantity }, { where: { id: assetId } });
+  await Client.update({ balance: newBalance }, { where: { id: clientId } });
+  await ClientAsset.update({ quantity: updateWallet }, { where: { clientId, assetId } });
 
-  const updateBalanceClient = await Client.update(
-    { balance: newBalance }, { where: { id: clientId } });
-
-  const updateAsset = await Asset.update(
-    { quantity: newQuantity }, { where: { id: assetId } });
-
-  return { status: 201, response: { codCliente: clientId, codAtivo: assetId, qtdeAtivo: quantity } };
+  return {
+    status: 201,
+    response: {
+      codCliente: clientId,
+      codAtivo: assetId,
+      qtdeAtivo: quantity
+    }
+  };
 };
 
 module.exports = {
