d3f4439e1466198e859c321d5aebf84704ad3943
Joice <joicemdsaguiar@gmail.com>
2022-07-24 19:53:54 -0300

    :hammer: refactor: added accountService changes

--- src/services/accountService.js
+++ src/services/accountService.js
@@ -4,13 +4,13 @@ const createDeposit = async ({ clientId, value }) => {
   const client = await Client.findOne({ where: { id: clientId } });
 
   if (!client) {
-    return res.status(404).json({ message: 'Client does not exist' }); 
+    return { status: 404, response: { message: 'Client not found' } }; 
   };
 
   const newBalance = Number(value) + Number(client.balance);
 
   if (value <= 0) {
-    return { status: 400, response: { message: 'The value you entered is not valid' } };
+    return { status: 400, response: { message: 'The value is invalid' } };
   };
 
   await Client.update({ balance: newBalance }, { where: { id: clientId } });
@@ -28,13 +28,13 @@ const createWithdrawal = async ({ clientId, value }) => {
   const client = await Client.findOne({ where: { id: clientId } });
 
   if (!client) {
-    return res.status(404).json({ message: 'Client does not exist' }); 
+    return { status: 404, response: { message: 'Client not found' } }; 
   };
 
   const newBalance = Number(client.balance) - Number(value);
 
   if (value <= 0 || value > client.balance) {
-    return { status: 400, response: { message: 'The value you entered is not valid' } };
+    return { status: 400, response: { message: 'The value is invalid' } };
   };
 
   await Client.update({ balance: newBalance }, { where: { id: clientId } });
@@ -52,7 +52,7 @@ const getByClientId = async (clientId) => {
   const client = await Client.findOne({ where: { id: clientId } });
 
   if (!client) {
-    return { status: 404, response: { message: 'Client does not exist' } };
+    return { status: 404, response: { message: 'Client not found' } };
   };
   
   return {
