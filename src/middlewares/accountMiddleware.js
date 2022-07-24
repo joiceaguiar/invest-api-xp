1d11458e4f1650f91d3ff88f9a7ab88d8cb050f5
Joice <joicemdsaguiar@gmail.com>
2022-07-24 19:53:02 -0300

    :hammer: refactor: added accountMiddleware changes

--- src/middlewares/accountMiddleware.js
+++ src/middlewares/accountMiddleware.js
@@ -2,7 +2,7 @@ const accountValidation = async (req, res, next) => {
   const { clientId, value } = req.body;
 
   if (!clientId || !value) {
-    return res.status(400).json({ message: 'Information is needed' });
+    return res.status(400).json({ message: 'Data is required' });
   }
 
   next();
