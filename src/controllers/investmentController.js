96ea04edb316647495cd3df34beeb6cc5f2594b0
Joice <joicemdsaguiar@gmail.com>
2022-07-23 21:09:28 -0300

    :sparkles: feat: added createSale controller

--- src/controllers/investmentController.js
+++ src/controllers/investmentController.js
@@ -9,6 +9,16 @@ const createPurchase = async (req, res) => {
   }
 };
 
+const createSale = async (req, res) => {
+  try {
+    const { status, response } = await investmentService.createSale(req.body);
+    return res.status(status).json(response);
+  } catch (error) {
+    return res.status(500).send('Internal Server Error');
+  }
+};
+
 module.exports = {
   createPurchase,
+  createSale,
 };
