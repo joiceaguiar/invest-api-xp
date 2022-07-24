228ae298ceb840f98b6e72ab744e97ed80c395e0
Joice <joicemdsaguiar@gmail.com>
2022-07-24 19:54:11 -0300

    :hammer: refactor: added assetService changes

--- src/services/assetService.js
+++ src/services/assetService.js
@@ -24,7 +24,7 @@ const getByAssetId = async (assetId) => {
   const asset = await Asset.findOne({ where: { id: assetId } });
 
   if (!asset) {
-    return { status: 404, response: { message: 'Asset does not exist' } };
+    return { status: 404, response: { message: 'Asset not found' } };
   };
 
   return {
